f819140e498572a2c53d50ab52065e2fabad15ae
xzgzszrh <xzgzszrh@outlook.com>
2025-01-02 13:34:53 +0800

    update

---
+++ public/chat/assets/crami-a44b5512.js
@@ -0,0 +1,1 @@
+import{g as a,p as e}from"./index-5b031a36.js";function c(r){return e({url:"/crami/useCrami",data:r})}function u(r){return a({url:"/crami/queryAllPackage",data:r})}export{c as a,u as f};
