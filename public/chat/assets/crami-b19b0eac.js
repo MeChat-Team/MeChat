e8414b370b4f00d872f2dd110e13640e110b038d
xzgzszrh <xzgzszrh@outlook.com>
2025-01-02 12:35:58 +0800

    upadte_chat

---
+++ public/chat/assets/crami-b19b0eac.js
@@ -0,0 +1,1 @@
+import{g as a,p as e}from"./index-9ab30c86.js";function c(r){return e({url:"/crami/useCrami",data:r})}function u(r){return a({url:"/crami/queryAllPackage",data:r})}export{c as a,u as f};
