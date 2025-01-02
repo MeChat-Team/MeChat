56279bafcfe20441c95ec8d15e57b84d23aebe89
xzgzszrh <xzgzszrh@outlook.com>
2025-01-03 01:09:51 +0800

    update

---
+++ public/chat/assets/crami-d0a6b2b2.js
@@ -0,0 +1,1 @@
+import{g as a,p as e}from"./index-4f77b0de.js";function c(r){return e({url:"/crami/useCrami",data:r})}function u(r){return a({url:"/crami/queryAllPackage",data:r})}export{c as a,u as f};
--- public/chat/assets/crami-f376489e.js
+++
@@ -1,1 +0,0 @@
-import{g as a,p as e}from"./index-e64573b8.js";function c(r){return e({url:"/crami/useCrami",data:r})}function u(r){return a({url:"/crami/queryAllPackage",data:r})}export{c as a,u as f};
