63d652b8147375378a74891330267d2654819197
xzgzszrh <xzgzszrh@outlook.com>
2025-01-03 15:26:13 +0800

    update

---
+++ public/chat/assets/crami-fb4739ba.js
@@ -0,0 +1,1 @@
+import{g as a,p as e}from"./index-a6387dc6.js";function c(r){return e({url:"/crami/useCrami",data:r})}function u(r){return a({url:"/crami/queryAllPackage",data:r})}export{c as a,u as f};
